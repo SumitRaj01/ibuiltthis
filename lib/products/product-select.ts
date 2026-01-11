d3dc1710277da7387ef99d22278864c3e1cb5ee6
SumitRaj01 <sumeet23051994@gmail.com>
2026-01-11 21:39:16 +0530

    dynamic product details page

--- lib/products/product-select.ts
+++ lib/products/product-select.ts
@@ -37,3 +37,11 @@ export async function getRecentlyLaunchedProducts() {
       new Date(product.createdAt.toISOString()) >= oneWeekAgo
   );
 }
+
+export async function getProductBySlug(slug: string) {
+  const product = await db
+    .select()
+    .from(products)
+    .where(eq(products.slug, slug));
+  return product?.[0] ?? null;
+}
